0ec51a2c7bae002f496c0d8b9eddcc408ad11e53
Bethan Helen <bethanhelen8@gmail.com>
2022-12-06 13:43:32 +0100

    additional pages

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./emy-tailwind-site/{index.html, style.css}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
